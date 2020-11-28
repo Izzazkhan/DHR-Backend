f733f0bd9bf3345dd3a1c5e56fb1aa93d61febec
Nomi77 <naeemtahir775@gmail.com>
2020-11-28 16:00:25 +0500

    image upload functionality added

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ dotenv.config({ path: './config/config.env' });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use('/uploads', express.static('uploads'));
 
 // Mounting Routes
 app.use('/api/dhrPatient', patientRouter);
