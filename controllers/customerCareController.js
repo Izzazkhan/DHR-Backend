820d57c5019c82fec72395b349d11fef3372bd02
Nomi77 <naeemtahir775@gmail.com>
2020-12-18 18:25:27 +0500

    customer care bug fixed

--- controllers/customerCareController.js
+++ controllers/customerCareController.js
@@ -12,13 +12,21 @@ exports.getAllCustomerCares = asyncHandler(async (req, res, next) => {
 });
 
 exports.assignCC = asyncHandler(async (req, res, next) => {
-  //   console.log(req.body);
+  console.log(req.body);
   const customerCareStaff = await Staff.findOne({ _id: req.body.staffId });
   if (!customerCareStaff || customerCareStaff.disabled === true) {
     return next(
-      new ErrorResponse('Could not assign Chief Complaint to this doctor', 400)
+      new ErrorResponse(
+        'Could not assign Chief Complaint to this Customer Care',
+        400
+      )
     );
   }
+  await Staff.findOneAndUpdate(
+    { _id: customerCareStaff.id },
+    { $set: { availability: false } },
+    { new: true }
+  );
   const customerCare = {
     assignedBy: req.body.assignedBy,
     customerCareId: req.body.staffId,
@@ -27,7 +35,7 @@ exports.assignCC = asyncHandler(async (req, res, next) => {
   };
   const assignedCC = await EDR.findOneAndUpdate(
     { _id: req.body.patientId },
-    { $push: { customerCare }, $set: { availability: false } },
+    { $push: { customerCare } },
     {
       new: true,
     }
