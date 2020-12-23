e64aa25dcfc8d79e0f93308f42783265363553ea
SarmadMir <sarmadmir13@gmail.com>
2020-12-23 17:32:01 +0500

    route naming

--- routes/dcdFormroutes.js
+++ routes/dcdFormroutes.js
@@ -5,13 +5,13 @@ const router = express.Router();
 const {
   addTriageAssessment,
   addDcdForm,
-  addPatinetDetals,
+  addPatientDetails,
   addPastHistory,
 } = require('../controllers/dcdFormController');
 
 router.put('/addTriageAssessment', addTriageAssessment);
 router.put('/addDcdForm', addDcdForm);
-router.put('/addPatientDetails', addPatinetDetals);
+router.put('/addPatientDetails', addPatientDetails);
 router.put('/addPastHistory', addPastHistory);
 
 module.exports = router;
