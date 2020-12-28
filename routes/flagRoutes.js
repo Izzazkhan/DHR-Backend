b2970baf2c47abd31fa4ae12d1064a8cf3c6f116
Mudassir Ijaz <ijazmudassir786@gmail.com>
2020-12-28 17:41:25 +0500

    flag controller

--- routes/flagRoutes.js
+++ routes/flagRoutes.js
@@ -7,11 +7,13 @@ const {
   getAllCompletedFlag,
   getAllPendingFlag,
   getFlagCount,
+  getFlagPatientByKeyword,
 } = require('../controllers/flagController');
 
 router.post('/addFlag', addFlag);
 router.get('/getAllCompletedFlag', getAllCompletedFlag);
 router.get('/getAllPendingFlag', getAllPendingFlag);
 router.get('/getFlagCount', getFlagCount);
+router.get('/getFlagPatientByKeyword/:keyword', getFlagPatientByKeyword);
 
 module.exports = router;
