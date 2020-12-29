4300b6ec1c16b7a03145431173d77f895be7b5c3
Nomi77 <naeemtahir775@gmail.com>
2020-12-29 20:45:17 +0500

    search for care stream added

--- controllers/careStreamController.js
+++ controllers/careStreamController.js
@@ -188,11 +188,56 @@ exports.asignCareStream = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.getCSPatientByKeyword = asyncHandler(async (req, res, next) => {
+exports.getPatientWithoutCSByKeyword = asyncHandler(async (req, res, next) => {
   const arr = [];
   const patients = await EDR.find({
     status: 'pending',
     careStream: { $eq: [] },
+    room: { $ne: [] },
+  }).populate('patientId');
+
+  for (let i = 0; i < patients.length; i++) {
+    const fullName =
+      patients[i].patientId.name[0].given[0] +
+      ' ' +
+      patients[i].patientId.name[0].family;
+    if (
+      (patients[i].patientId.name[0].given[0] &&
+        patients[i].patientId.name[0].given[0]
+          .toLowerCase()
+          .startsWith(req.params.keyword.toLowerCase())) ||
+      (patients[i].patientId.name[0].family &&
+        patients[i].patientId.name[0].family
+          .toLowerCase()
+          .startsWith(req.params.keyword.toLowerCase())) ||
+      (patients[i].patientId.identifier[0].value &&
+        patients[i].patientId.identifier[0].value
+          .toLowerCase()
+          .startsWith(req.params.keyword.toLowerCase())) ||
+      fullName.toLowerCase().startsWith(req.params.keyword.toLowerCase()) ||
+      (patients[i].patientId.telecom[1].value &&
+        patients[i].patientId.telecom[1].value
+          .toLowerCase()
+          .startsWith(req.params.keyword.toLowerCase())) ||
+      (patients[i].patientId.nationalID &&
+        patients[i].patientId.nationalID
+          .toLowerCase()
+          .startsWith(req.params.keyword.toLowerCase()))
+    ) {
+      arr.push(patients[i]);
+    }
+  }
+  res.status(200).json({
+    success: true,
+    data: arr,
+  });
+});
+
+exports.getPatientsWithCSByKeyword = asyncHandler(async (req, res, next) => {
+  const arr = [];
+  const patients = await EDR.find({
+    status: 'pending',
+    careStream: { $ne: [] },
     room: { $ne: [] },
   }).populate('patientId');
 
@@ -238,7 +283,9 @@ exports.getEDRswithCS = asyncHandler(async (req, res, next) => {
     status: 'pending',
     careStream: { $ne: [] },
     room: { $ne: [] },
-  }).populate('patientId');
+  }).populate(
+    'patientId productionArea.productionAreaId careStream.careStreamId'
+  );
 
   res.status(200).json({
     success: true,
