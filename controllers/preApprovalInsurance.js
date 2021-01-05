71291459cfd74f677699786769e5bfb47b22373f
Nomi77 <naeemtahir775@gmail.com>
2021-01-05 21:32:30 +0500

    par compledted

--- controllers/preApprovalInsurance.js
+++ controllers/preApprovalInsurance.js
@@ -1,9 +1,11 @@
-// const requestNoFormat = require('dateformat');
+const requestNoFormat = require('dateformat');
 const asyncHandler = require('../middleware/async');
 // const CareStream = require('../models/CareStreams/CareStreams');
 const EDR = require('../models/EDR/EDR');
-// const IV = require('../models/insuranceVendor');
-// const IT = require('../models/insuranceItem');
+const IV = require('../models/insuranceVendor');
+const IT = require('../models/insuranceItem');
+const PAR = require('../models/par');
+const ErrorResponse = require('../utils/errorResponse');
 
 exports.getPreApprovalEDR = asyncHandler(async (req, res) => {
   const edr = await EDR.find({
@@ -99,4 +101,57 @@ exports.getEDRandIPRKeyword = asyncHandler(async (req, res) => {
     success: true,
     data: arr,
   });
+});
+
+exports.addPAR = asyncHandler(async (req, res) => {
+  const {
+    edrId,
+    iprId,
+    // oprId,
+    generatedBy,
+    patient,
+    pharId,
+    lrId,
+    rrId,
+    nsId,
+    generatedFrom,
+    generatedFromSub,
+    approvalNo,
+    approvalPerson,
+    comments,
+    coPayment,
+    netPayment,
+    status,
+  } = req.body;
+  const par = await PAR.create({
+    requestNo: 'PA' + requestNoFormat(new Date(), 'mmddyyHHmm'),
+    edrId,
+    iprId,
+    // oprId,
+    generatedBy,
+    patient,
+    pharId,
+    lrId,
+    rrId,
+    nsId,
+    generatedFrom,
+    generatedFromSub,
+    approvalNo,
+    approvalPerson,
+    comments,
+    coPayment,
+    netPayment,
+    status,
+  });
+  res.status(200).json({ success: true, data: par });
+});
+
+exports.updatePAR = asyncHandler(async (req, res, next) => {
+  const { _id } = req.body;
+  let par = await PAR.findById(_id);
+  if (!par) {
+    return next(new ErrorResponse(`PAR not found with id of ${_id}`, 404));
+  }
+  par = await PAR.updateOne({ _id: _id }, req.body);
+  res.status(200).json({ success: true, data: par });
 });
