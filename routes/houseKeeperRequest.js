eb7eac45e5abf44bb791ce9b070eb520572f52ef
Nomi77 <naeemtahir775@gmail.com>
2021-01-18 20:35:58 +0500

    housekeeping for sensei added

--- routes/houseKeeperRequest.js
+++ routes/houseKeeperRequest.js
@@ -3,12 +3,16 @@ const express = require('express');
 const {
   pendingRadHouseKeeperRequests,
   comletedRadHouseKeeperRequests,
+  pendingSenseiHouseKeeperRequests,
+  comletedSenseiHouseKeeperRequests,
   updateStatus,
 } = require('../controllers/houseKeeperRequest');
 
 const router = express.Router();
 
 router.get('/getPendingRadHKRequests', pendingRadHouseKeeperRequests);
+router.get('/pendingSenseiHKRequests', pendingSenseiHouseKeeperRequests);
+router.get('/completedSenseiHKRequests', comletedSenseiHouseKeeperRequests);
 router.get('/getCompletedRadHKRequests', comletedRadHouseKeeperRequests);
 router.put('/updateStatus', updateStatus);
 
