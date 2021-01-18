1ddc6a46628a69cf2d6dbcdcf7791e22cd692c70
Nomi77 <naeemtahir775@gmail.com>
2021-01-18 21:02:19 +0500

    room availability updated

--- controllers/houseKeeperRequest.js
+++ controllers/houseKeeperRequest.js
@@ -1,6 +1,7 @@
 // const Staff = require('../models/staffFhir/staff');
 const HK = require('../models/houseKeepingRequest');
 const asyncHandler = require('../middleware/async');
+const Room = require('../models/room');
 
 exports.pendingRadHouseKeeperRequests = asyncHandler(async (req, res, next) => {
   const HKRequests = await HK.find({
@@ -77,6 +78,26 @@ exports.updateStatus = asyncHandler(async (req, res, next) => {
     { new: true }
   );
 
+  res.status(200).json({
+    success: true,
+    data: updatedStatus,
+  });
+});
+
+exports.updateSenseiStatus = asyncHandler(async (req, res, next) => {
+  const updatedStatus = await HK.findOneAndUpdate(
+    { _id: req.body.requestId },
+    {
+      $set: { status: 'completed', task: req.body.task, updateAt: Date.now() },
+    },
+    { new: true }
+  );
+
+  await Room.findOneAndUpdate(
+    { _id: req.body.roomId },
+    { $set: { availability: true } },
+    { $new: true }
+  );
   res.status(200).json({
     success: true,
     data: updatedStatus,
