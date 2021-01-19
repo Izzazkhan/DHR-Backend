6a731bfcba0f51dffe348a1c63529f8d791984f8
Nomi77 <naeemtahir775@gmail.com>
2021-01-19 20:21:52 +0500

    changes

--- controllers/radRequest.js
+++ controllers/radRequest.js
@@ -81,17 +81,9 @@ exports.getCompletedRadEdr = asyncHandler(async (req, res, next) => {
     {
       $unwind: '$radRequest',
     },
-    // {
-    //   $match: {
-    //     'radRequest.status': 'completed',
-    //   },
-    // },
     {
       $match: {
-        $or: [
-          { 'radRequest.status': 'completed' },
-          { 'radRequest.status': 'pending approval' }
-        ]
+        'radRequest.status': 'completed',
       },
     },
   ]);
@@ -133,12 +125,24 @@ exports.getCompletedRadEdr = asyncHandler(async (req, res, next) => {
 exports.updateRadRequest = asyncHandler(async (req, res, next) => {
   console.log(req.files);
   const parsed = JSON.parse(req.body.data);
-
   const rad = await EDR.findOne({ _id: parsed.edrId });
   let note;
   for (let i = 0; i < rad.radRequest.length; i++) {
     if (rad.radRequest[i]._id == parsed.radId) {
+      // console.log(i);
       note = i;
+    }
+  }
+  let voiceNotes;
+  const arr = [];
+  if (req.files) {
+    for (let i = 0; i < req.files.length; i++) {
+      if (req.files[i].mimetype.includes('image')) {
+        arr.push(req.files[i].path);
+      }
+      if (req.files[i].mimetype.includes('audio')) {
+        voiceNotes = req.files[i].path;
+      }
     }
   }
 
@@ -153,7 +157,6 @@ exports.updateRadRequest = asyncHandler(async (req, res, next) => {
     { new: true }
   );
 
-<<<<<<< HEAD
   const updatedrad = await EDR.findOneAndUpdate(
     { _id: parsed.edrId },
     {
@@ -164,58 +167,15 @@ exports.updateRadRequest = asyncHandler(async (req, res, next) => {
         [`radRequest.${note}.completeTime`]: parsed.completeTime,
         [`radRequest.${note}.holdTime`]: parsed.holdTime,
         [`radRequest.${note}.voiceNotes`]: voiceNotes,
-        [`radRequest.${note}.image`]: req.files.mimetype.includes('image')
-          ? arr
-          : parsed.radRequest,
-=======
-  if (parsed.staffType === "Doctor") {
-
-    const updatedrad = await EDR.findOneAndUpdate(
-      { _id: parsed.edrId },
-      {
-        $set: {
-          [`radRequest.${note}.status`]: parsed.status,
-          [`radRequest.${note}.completeTime`]: parsed.completeTime,
-          [`radRequest.${note}.voiceNotes`]: req.files[0].path
-        },
->>>>>>> 1972781c40718c14e60bd1626501c4a811da8d35
+        [`radRequest.${note}.image`]: arr,
       },
-      { new: true }
-    ).populate('radRequest.serviceId');
-
-    res.status(200).json({
-      success: true,
-      data: updatedrad,
-    });
-  } else {
-
-    const arr = [];
-    if (req.files) {
-      for (let i = 0; i < req.files.length; i++) {
-        arr.push(req.files[i].path);
-      }
-    }
-
-    const updatedrad = await EDR.findOneAndUpdate(
-      { _id: parsed.edrId },
-      {
-        $set: {
-          [`radRequest.${note}.status`]: parsed.status,
-          [`radRequest.${note}.delayedReason`]: parsed.delayedReason,
-          [`radRequest.${note}.activeTime`]: parsed.activeTime,
-          [`radRequest.${note}.pendingApprovalTime`]: parsed.pendingApprovalTime,
-          [`radRequest.${note}.holdTime`]: parsed.holdTime,
-          [`radRequest.${note}.image`]: arr,
-        },
-      },
-      { new: true }
-    ).populate('radRequest.serviceId');
-
-    res.status(200).json({
-      success: true,
-      data: updatedrad,
-    });
-  }
+    },
+    { new: true }
+  ).populate('radRequest.serviceId');
+  res.status(200).json({
+    success: true,
+    data: updatedrad,
+  });
 });
 
 exports.searchPendingRadRequest = asyncHandler(async (req, res, next) => {
@@ -340,113 +300,5 @@ exports.assignHouseKeeper = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: assignedHK,
-  });
-});
-
-exports.getPendingRadEdrForED = asyncHandler(async (req, res, next) => {
-  const unwindEdr = await EDR.aggregate([
-    {
-      $project: {
-        _id: 1,
-        radRequest: 1,
-        room: 1,
-        patientId: 1,
-        chiefComplaint: 1,
-      },
-    },
-    {
-      $unwind: '$radRequest',
-    },
-    {
-      $match: {
-        'radRequest.status': 'pending approval',
-      },
-    },
-  ]);
-
-  const edrs = await EDR.populate(unwindEdr, [
-    {
-      path: 'chiefComplaint.chiefComplaintId',
-      model: 'chiefComplaint',
-      select: 'chiefComplaintId',
-      populate: [
-        {
-          path: 'productionArea.productionAreaId',
-          model: 'productionArea',
-          select: 'paName',
-        },
-      ],
-    },
-    {
-      path: 'patientId',
-      model: 'patientfhir',
-    },
-    {
-      path: 'radRequest.serviceId',
-      model: 'RadiologyService',
-    },
-    {
-      path: 'room.roomId',
-      model: 'room',
-      select: 'roomNo',
-    },
-  ]);
-  res.status(200).json({
-    success: true,
-    data: edrs,
-  });
-});
-
-exports.getCompletedRadEdrForED = asyncHandler(async (req, res, next) => {
-  const unwindEdr = await EDR.aggregate([
-    {
-      $project: {
-        _id: 1,
-        radRequest: 1,
-        room: 1,
-        patientId: 1,
-        chiefComplaint: 1,
-      },
-    },
-    {
-      $unwind: '$radRequest',
-    },
-    {
-      $match: {
-        'radRequest.status': 'completed',
-      },
-    },
-  ]);
-
-  const edrs = await EDR.populate(unwindEdr, [
-    {
-      path: 'chiefComplaint.chiefComplaintId',
-      model: 'chiefComplaint',
-      select: 'chiefComplaintId',
-      populate: [
-        {
-          path: 'productionArea.productionAreaId',
-          model: 'productionArea',
-          select: 'paName',
-        },
-      ],
-    },
-    {
-      path: 'patientId',
-      model: 'patientfhir',
-    },
-    {
-      path: 'radRequest.serviceId',
-      model: 'RadiologyService',
-    },
-    {
-      path: 'room.roomId',
-      model: 'room',
-      select: 'roomNo',
-    },
-  ]);
-  res.status(200).json({
-    success: true,
-    data: edrs,
   });
 });
