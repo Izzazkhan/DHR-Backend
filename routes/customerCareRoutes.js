5eb4a613f2b61336dbe1f57e297c85d5e4c6bb43
Nomi77 <naeemtahir775@gmail.com>
2021-01-26 17:37:27 +0500

    complete transfer update and get added

--- routes/customerCareRoutes.js
+++ routes/customerCareRoutes.js
@@ -7,11 +7,15 @@ const {
   getCCStaffByKeyword,
   assignCC,
   pendingEdToEouTransfers,
+  completeTransfer,
+  completedEdToEouTransfers,
 } = require('../controllers/customerCareController');
 
 router.get('/getAllCustomerCares', getAllCustomerCares);
 router.get('/searchCustomerCare/:keyword', getCCStaffByKeyword);
 router.put('/assignCustomerCare', assignCC);
+router.get('/completeTransfer/:transferId', completeTransfer);
 router.get('/pendingEouTransfer/:ccId', pendingEdToEouTransfers);
+router.get('/completedEouTransfer/:ccId', completedEdToEouTransfers);
 
 module.exports = router;
