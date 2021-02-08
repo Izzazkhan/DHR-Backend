7f6c98c8abb0a6b9d4cb2c7f53990796a19fbf3d
Muhammad Saad <saad.ahamd201097@gmail.com>
2021-02-08 23:59:33 +0500

    transaction reports

--- routes/patientClearance.js
+++ routes/patientClearance.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const {
-    getPatientClearance,
-    getPatientClearanceById,
-    addPatientClearance,
-    updatePatientClearance
-  } = require('../controllers/patientClearance');
+  getPatientClearance,
+  getPatientClearanceById,
+  addPatientClearance,
+  updatePatientClearance,
+  getClearedPatients,
+  searchClearedPatients
+} = require('../controllers/patientClearance');
 
 const router = express.Router();
 router.get('/getpatientclearance', getPatientClearance);
+router.get('/getClearedPatients', getClearedPatients);
+router.get('/searchClearedPatients/:keyword', searchClearedPatients);
 router.get('/getpatientclearance/:id', getPatientClearanceById);
 router.post('/addpatientclearance', addPatientClearance);
 router.put('/updatepatientclearance', updatePatientClearance);
