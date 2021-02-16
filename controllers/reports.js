79d8a5f003819254674d814c547148f9e38d7238
Naeem775 <naeemtahir775@gmail.com>
2021-02-16 18:42:38 +0500

    data for house keeper request added

--- controllers/reports.js
+++ controllers/reports.js
@@ -1,6 +1,7 @@
 const moment = require('moment');
 const Patient = require('../models/patient/patient');
 const Room = require('../models/room');
+const HKRequests = require('../models/houseKeepingRequest');
 
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
@@ -16,20 +17,20 @@ exports.roDashboard = asyncHandler(async (req, res) => {
   const secondHour = moment().subtract(5, 'hours').utc().toDate();
   const sixHour = moment().subtract(6, 'hours').utc().toDate();
 
-  // *  Registrations Per Hour
+  // * Register Officer Registrations Per Hour
   const patients = await Patient.find({
-    // 'processTime.processName': 'Registration Officer',
+    'processTime.processName': 'Registration Officer',
     $and: [
       { 'processTime.processStartTime': { $gte: sixHour } },
       { 'processTime.processEndTime': { $lte: currentTime } },
     ],
-  });
+  }).countDocuments();
 
-  const averageRegistrationTime = 360 / patients.length;
+  const averageRegistrationTime = 360 / patients;
   const arr = [];
 
   const sixthHourPatient = await Patient.find({
-    // 'processTime.processName': 'Registration Officer',
+    'processTime.processName': 'Registration Officer',
     $and: [
       { 'processTime.processStartTime': { $gte: lastHour } },
       { 'processTime.processEndTime': { $lte: currentTime } },
@@ -39,13 +40,13 @@ exports.roDashboard = asyncHandler(async (req, res) => {
   arr.push({ label: lastHour, value: 5 });
 
   const fifthHourPatient = await Patient.find({
-    // 'processTime.processName': 'Registration Officer',
+    'processTime.processName': 'Registration Officer',
     $and: [
       { 'processTime.processStartTime': { $gte: fifthHour } },
       { 'processTime.processEndTime': { $lte: lastHour } },
     ],
   }).countDocuments();
-  // arr.push({ label: fifthHour, value: fifthHourPatient });
+  arr.push({ label: fifthHour, value: fifthHourPatient });
   arr.push({ label: fifthHour, value: 4 });
   const fourthHourPatient = await Patient.find({
     // 'processTime.processName': 'Registration Officer',
@@ -58,7 +59,7 @@ exports.roDashboard = asyncHandler(async (req, res) => {
   arr.push({ label: fourthHour, value: 10 });
 
   const thirdHourPatient = await Patient.find({
-    // 'processTime.processName': 'Registration Officer',
+    'processTime.processName': 'Registration Officer',
     $and: [
       { 'processTime.processStartTime': { $gte: thirdHour } },
       { 'processTime.processEndTime': { $lte: fourthHour } },
@@ -68,7 +69,7 @@ exports.roDashboard = asyncHandler(async (req, res) => {
   arr.push({ label: thirdHour, value: 12 });
 
   const secondHourPatient = await Patient.find({
-    // 'processTime.processName': 'Registration Officer',
+    'processTime.processName': 'Registration Officer',
     $and: [
       { 'processTime.processStartTime': { $gte: secondHour } },
       { 'processTime.processEndTime': { $lte: thirdHour } },
@@ -78,7 +79,7 @@ exports.roDashboard = asyncHandler(async (req, res) => {
   arr.push({ label: secondHour, value: 9 });
 
   const firstHourPatient = await Patient.find({
-    // 'processTime.processName': 'Registration Officer',
+    'processTime.processName': 'Registration Officer',
     $and: [
       { 'processTime.processStartTime': { $gte: sixHour } },
       { 'processTime.processEndTime': { $lte: secondHour } },
@@ -90,14 +91,80 @@ exports.roDashboard = asyncHandler(async (req, res) => {
   //   * Pending Registration After Sensei
   const pendingSensei = await Patient.find({
     'processTime.processName': 'Sensei',
-    registrationStatus: 'pending',
+    // registrationStatus: 'pending',
     $and: [
       { 'processTime.processStartTime': { $gte: sixHour } },
       // { 'processTime.processEndTime': { $lte: currentTime } },
     ],
   });
 
-  const averageSenseiRegisterTime = 360 / pendingSensei.length;
+  const totalSenseiPending = pendingSensei.length;
+
+  const averageSenseiRegisterTime = 360 / totalSenseiPending;
+
+  // * Sensei Per Hour
+
+  const senseiArr = [];
+
+  const sixthHourPatientSensei = await Patient.find({
+    'processTime.processName': 'Sensei',
+    registrationStatus: 'pending',
+    $and: [
+      { 'processTime.processStartTime': { $gte: lastHour } },
+      { 'processTime.processEndTime': { $lte: currentTime } },
+    ],
+  }).countDocuments();
+  // senseiArr.push({ label: lastHour, value: sixthHourPatientSensei });
+  senseiArr.push({ label: lastHour, value: 5 });
+
+  const fifthHourPatientSensei = await Patient.find({
+    'processTime.processName': 'Registration Officer',
+    $and: [
+      { 'processTime.processStartTime': { $gte: fifthHour } },
+      { 'processTime.processEndTime': { $lte: lastHour } },
+    ],
+  }).countDocuments();
+  // senseiArr.push({ label: fifthHour, value: fifthHourPatientSensei });
+  senseiArr.push({ label: fifthHour, value: 4 });
+  const fourthHourPatientSensei = await Patient.find({
+    'processTime.processName': 'Registration Officer',
+    $and: [
+      { 'processTime.processStartTime': { $gte: fourthHour } },
+      { 'processTime.processEndTime': { $lte: fifthHour } },
+    ],
+  }).countDocuments();
+  // senseiArr.push({ label: fourthHour, value: fourthHourPatientSensei });
+  senseiArr.push({ label: fourthHour, value: 10 });
+
+  const thirdHourPatientSensei = await Patient.find({
+    'processTime.processName': 'Registration Officer',
+    $and: [
+      { 'processTime.processStartTime': { $gte: thirdHour } },
+      { 'processTime.processEndTime': { $lte: fourthHour } },
+    ],
+  }).countDocuments();
+  // senseiArr.push({ label: thirdHour, value: thirdHourPatientSensei });
+  senseiArr.push({ label: thirdHour, value: 12 });
+
+  const secondHourPatientSensei = await Patient.find({
+    'processTime.processName': 'Registration Officer',
+    $and: [
+      { 'processTime.processStartTime': { $gte: secondHour } },
+      { 'processTime.processEndTime': { $lte: thirdHour } },
+    ],
+  }).countDocuments();
+  // senseiArr.push({ label: secondHour, value: secondHourPatientSensei });
+  senseiArr.push({ label: secondHour, value: 9 });
+
+  const firstHourPatientSensei = await Patient.find({
+    'processTime.processName': 'Registration Officer',
+    $and: [
+      { 'processTime.processStartTime': { $gte: sixHour } },
+      { 'processTime.processEndTime': { $lte: secondHour } },
+    ],
+  }).countDocuments();
+  // senseiArr.push({ label: sixHour, value: firstHourPatientSensei });
+  senseiArr.push({ label: sixHour, value: 1 });
 
   // Available ED Beds
   const EdBeds = await Room.find({
@@ -125,18 +192,90 @@ exports.roDashboard = asyncHandler(async (req, res) => {
     totalUnInsured: edrUnInsured,
     availableEdBeds: EdBeds,
     cumulativeRegistrations: totalRegistrations,
-    // registrationPerHour: {
-    //   averageTAT: averageRegistrationTime,
-    //   sixthHourPatient,
-    //   fifthHourPatient,
-    //   fourthHourPatient,
-    //   thirdHourPatient,
-    //   secondHourPatient,
-    //   firstHourPatient,
-    // },
     registrationPerHour: arr,
+    averageTAT: averageRegistrationTime,
+    registeredPatients: patients,
     pendingRegistrationSensei: {
       averageTAT: averageSenseiRegisterTime,
+      totalSenseiPending,
+    },
+  });
+});
+
+// House Keeper Dashboard
+exports.hkDashboard = asyncHandler(async (req, res, next) => {
+  const currentTime = moment().utc().toDate();
+  const sixHour = moment().subtract(6, 'hours').utc().toDate();
+
+  // Room Cleaning Pending
+  const roomPending = await HKRequests.find({
+    status: 'pending',
+    assignedTime: { $gte: sixHour },
+  }).countDocuments();
+
+  const averageRoomTAT = 360 / roomPending;
+
+  // Room Cleaning Completed
+  const roomComplete = await HKRequests.find({
+    status: 'completed',
+    completedAt: { $gte: sixHour },
+  }).countDocuments();
+
+  const averageCompleteRoomTAT = 360 / roomComplete;
+
+  // Cumulative total Beds Cleaned
+  const totalCleanedBeds = await HKRequests.find({
+    status: 'completed',
+    task: 'Cleaned',
+  }).countDocuments();
+
+  res.status(200).json({
+    status: 'Success',
+    pendingRoom: {
+      averageRoomTAT,
+      totalPending: roomPending,
+    },
+    completeRoom: {
+      averageCompleteRoomTAT,
+      totalCompleted: roomComplete,
+    },
+    totalCleanedBeds,
+  });
+});
+
+// Anesthesiologist Dashboard
+exports.swDashboard = asyncHandler(async (req, res, next) => {
+  const currentTime = moment().utc().toDate();
+  const sixHour = moment().subtract(6, 'hours').utc().toDate();
+  // const totalPending = await EDR.find({ anesthesiologistNote: { $ne: [] } });
+  const pending = await EDR.aggregate([
+    {
+      $project: {
+        anesthesiologistNote: 1,
+      },
+    },
+    {
+      $match: {
+        anesthesiologistNote: { $ne: [] },
+      },
+    },
+    {
+      $unwind: '$anesthesiologistNote',
+    },
+    {
+      $match: {
+        $and: [{ status: 'pending' }, { noteTime: { $gte: sixHour } }],
+      },
+    },
+  ]);
+
+  const pendingTat = 360 / pending.length;
+
+  res.status(200).json({
+    status: 'Success',
+    totalRequests: {
+      pendingTat,
+      totalPending: pending.length,
     },
   });
 });
--- routes/reports.js
+++ routes/reports.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-const { roDashboard } = require('../controllers/reports');
+const {
+  roDashboard,
+  hkDashboard,
+  swDashboard,
+} = require('../controllers/reports');
 
 router.get('/roDashboard', roDashboard);
+router.get('/hkDashboard', hkDashboard);
+router.get('/swDashboard', swDashboard);
 
 module.exports = router;
