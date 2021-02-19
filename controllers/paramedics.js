edb17a3e538d9b5fdc2cadab8dc527b2e914ec19
Muhammad Saad <saad.ahamd201097@gmail.com>
2021-02-19 23:51:21 +0500

    paramdecis done

--- controllers/paramedics.js
+++ controllers/paramedics.js
@@ -257,3 +257,85 @@ exports.searchPMEdr = asyncHandler(async (req, res, next) => {
     data: arr,
   });
 });
+
+exports.criticalCasesPerParamedics = asyncHandler(async (req, res, next) => {
+  const paramedicsEdr = await EDR.find({
+    generatedFrom: 'Paramedics',
+    generatedBy: { $exists: true },
+  });
+
+  const staff = await Staff.find({ staffType: 'Paramedics' }).populate(
+    'addedBy'
+  );
+  let resWithCount = [];
+  for (let i = 0; i < staff.length; i++) {
+    let casesHandled = 0;
+    for (let j = 0; j < paramedicsEdr.length; j++) {
+      if (staff[i]._id.toString() == paramedicsEdr[j].generatedBy.toString()) {
+        casesHandled++;
+      }
+    }
+    let obj = JSON.parse(JSON.stringify(staff[i]));
+    obj.casesHandled = casesHandled;
+    resWithCount.push(obj);
+  }
+  res.status(200).json({
+    success: true,
+    data: resWithCount,
+  });
+});
+
+exports.searchParamedicsByCriricalCases = asyncHandler(async (req, res, next) => {
+  const arr = [];
+  const paramedicsEdr = await EDR.find({
+    generatedFrom: 'Paramedics',
+    generatedBy: { $exists: true },
+  });
+
+  const s = await Staff.find({ staffType: 'Paramedics' }).populate('addedBy');
+  let staff = [];
+  for (let i = 0; i < s.length; i++) {
+    let casesHandled = 0;
+    for (let j = 0; j < paramedicsEdr.length; j++) {
+      if (s[i]._id.toString() == paramedicsEdr[j].generatedBy.toString()) {
+        casesHandled++;
+      }
+    }
+    let obj = JSON.parse(JSON.stringify(s[i]));
+    obj.casesHandled = casesHandled;
+    staff.push(obj);
+  }
+
+  for (let i = 0; i < staff.length; i++) {
+    const fullName = staff[i].name[0].given[0] + ' ' + staff[i].name[0].family;
+    if (
+      (staff[i].name[0].given[0] &&
+        staff[i].name[0].given[0]
+          .toLowerCase()
+          .startsWith(req.params.keyword.toLowerCase())) ||
+      (staff[i].name[0].family &&
+        staff[i].name[0].family
+          .toLowerCase()
+          .startsWith(req.params.keyword.toLowerCase())) ||
+      (staff[i].identifier[0].value &&
+        staff[i].identifier[0].value
+          .toLowerCase()
+          .startsWith(req.params.keyword.toLowerCase())) ||
+      fullName.toLowerCase().startsWith(req.params.keyword.toLowerCase()) ||
+      (staff[i].telecom[1].value &&
+        staff[i].telecom[1].value
+          .toLowerCase()
+          .startsWith(req.params.keyword.toLowerCase())) ||
+      (staff[i].nationalID &&
+        staff[i].nationalID
+          .toLowerCase()
+          .startsWith(req.params.keyword.toLowerCase()))
+    ) {
+      arr.push(staff[i]);
+    }
+  }
+  res.status(200).json({
+    success: true,
+    data: arr,
+  });
+});
