117d6f4c829c35b67689b74faa0073096646b521
SarmadMir <sarmadmir13@gmail.com>
2021-02-21 00:53:17 +0500

    Admin Panel Dashboard Most selected Requests/Data calculated

--- controllers/adminDashboard.js
+++ controllers/adminDashboard.js
@@ -5,6 +5,9 @@ const EDR = require('../models/EDR/EDR');
 const ChiefComplaints = require('../models/chiefComplaint/chiefComplaint');
 const CareStreams = require('../models/CareStreams/CareStreams');
 const LabServices = require('../models/service/lab')
+const RadServices = require('../models/service/radiology')
+const Item = require('../models/item')
+const Vendor = require('../models/vendor')
 
 // Most Selected Chief Complaints
 exports.chiefComplaints = asyncHandler(async (req, res) => {
@@ -20,14 +23,9 @@ exports.chiefComplaints = asyncHandler(async (req, res) => {
             $unwind: '$chiefComplaint',
         },
         {
-            $match: {
-                $and: [
-                    { 'chiefComplaint.assignedTime': { $gte: sixHour } }
-                ],
-            },
+            $match: { 'chiefComplaint.assignedTime': { $gte: sixHour } }
         },
     ]);
-    // console.log("Chief Complaints assigned in last 6 hours : ", assignedCareStreams)
     const chiefComplaints = await ChiefComplaints.aggregate([
         {
             $match: {
@@ -44,7 +42,6 @@ exports.chiefComplaints = asyncHandler(async (req, res) => {
             },
         },
     ])
-    // console.log("Total Chief Complaints : ", chiefComplaints)
     for (let i = 0; i < chiefComplaints.length; i++) {
         if (assignedCareStreams.length > 0) {
             for (let j = 0; j < assignedCareStreams.length; j++) {
@@ -65,7 +62,6 @@ exports.chiefComplaints = asyncHandler(async (req, res) => {
             chiefComplaints[i].selected = 0
         }
     }
-    // console.log("chief complaints data selected : ", chiefComplaints)
     res.status(200).json({
         success: true,
         data: chiefComplaints
@@ -86,21 +82,12 @@ exports.careStreams = asyncHandler(async (req, res) => {
             $unwind: '$careStream',
         },
         {
-            $match: {
-                $and: [
-                    { 'careStream.assignedTime': { $gte: sixHour } }
-                ],
-            },
+            $match: { 'careStream.assignedTime': { $gte: sixHour } }
         },
     ]);
-    // console.log("careStream assigned in last 6 hours : ", assignedCareStreams)
     const careStreams = await CareStreams.aggregate([
         {
-            $match: {
-                $and: [
-                    { 'disabled': false },
-                ],
-            },
+            $match: { 'disabled': false },
         },
         {
             $project: {
@@ -109,7 +96,6 @@ exports.careStreams = asyncHandler(async (req, res) => {
             },
         },
     ])
-    // console.log("Total CareStreams : ", careStreams)
     for (let i = 0; i < careStreams.length; i++) {
         if (assignedCareStreams.length > 0) {
             for (let j = 0; j < assignedCareStreams.length; j++) {
@@ -130,7 +116,6 @@ exports.careStreams = asyncHandler(async (req, res) => {
             careStreams[i].selected = 0
         }
     }
-    // console.log("chief complaints data selected : ", careStreams)
     res.status(200).json({
         success: true,
         data: careStreams
@@ -151,21 +136,12 @@ exports.labTests = asyncHandler(async (req, res) => {
             $unwind: '$labRequest',
         },
         {
-            $match: {
-                $and: [
-                    { 'labRequest.requestedAt': { $gte: sixHour } }
-                ],
-            },
+            $match: { 'labRequest.requestedAt': { $gte: sixHour } }
         },
     ]);
-    // console.log("services in last 6 hours : ", requestedLabReq)
     const labServices = await LabServices.aggregate([
         {
-            $match: {
-                $and: [
-                    { 'disabled': false },
-                ],
-            },
+            $match: { 'disabled': false },
         },
         {
             $project: {
@@ -175,8 +151,8 @@ exports.labTests = asyncHandler(async (req, res) => {
             },
         },
     ])
-    console.log("Total labServices : ", labServices)
     for (let i = 0; i < labServices.length; i++) {
+        labServices[i].lastRequested = ''
         if (requestedLabReq.length > 0) {
             for (let j = 0; j < requestedLabReq.length; j++) {
                 if (labServices[i]._id.toString() === requestedLabReq[j].labRequest.serviceId.toString()) {
@@ -186,8 +162,14 @@ exports.labTests = asyncHandler(async (req, res) => {
                     else {
                         labServices[i].selected++
                     }
+                    if (labServices[i].lastRequested = '') {
+                        labServices[i].lastRequested = requestedLabReq[j].labRequest.requestedAt
+                    }
+                    else if (labServices[i].lastRequested <= requestedLabReq[j].labRequest.requestedAt) {
+                        labServices[i].lastRequested = requestedLabReq[j].labRequest.requestedAt
+                    }
                 }
-                else if (!labServices[i].selected){
+                else if (!labServices[i].selected) {
                     labServices[i].selected = 0
                 }
             }
@@ -196,9 +178,440 @@ exports.labTests = asyncHandler(async (req, res) => {
             labServices[i].selected = 0
         }
     }
-    // console.log("Lab services selected : ", labServices)
     res.status(200).json({
         success: true,
         data: labServices
+    });
+});
+
+// Most Requested Rad Tests
+exports.radiologyExams = asyncHandler(async (req, res) => {
+    const sixHour = moment().subtract(6, 'hours').utc().toDate();
+    // Patients who requested rad service in the last 6 hours
+    const requestedRadReq = await EDR.aggregate([
+        {
+            $project: {
+                radRequest: 1
+            },
+        },
+        {
+            $unwind: '$radRequest',
+        },
+        {
+            $match: { 'radRequest.requestedAt': { $gte: sixHour } }
+        },
+    ]);
+    const radServices = await RadServices.aggregate([
+        {
+            $match: { 'disabled': false },
+        },
+        {
+            $project: {
+                name: 1,
+                identifier: 1,
+                type: 1
+            },
+        },
+    ])
+    for (let i = 0; i < radServices.length; i++) {
+        radServices[i].lastRequested = ''
+        if (requestedRadReq.length > 0) {
+            for (let j = 0; j < requestedRadReq.length; j++) {
+                if (radServices[i]._id.toString() === requestedRadReq[j].radRequest.serviceId.toString()) {
+                    if (!radServices[i].selected) {
+                        radServices[i].selected = 1
+                    }
+                    else {
+                        radServices[i].selected++
+                    }
+                    if (radServices[i].lastRequested = '') {
+                        radServices[i].lastRequested = requestedRadReq[j].radRequest.requestedAt
+                    }
+                    else if (radServices[i].lastRequested <= requestedRadReq[j].radRequest.requestedAt) {
+                        radServices[i].lastRequested = requestedRadReq[j].radRequest.requestedAt
+                    }
+                }
+                else if (!radServices[i].selected) {
+                    radServices[i].selected = 0
+                }
+            }
+        }
+        else {
+            radServices[i].selected = 0
+        }
+    }
+    res.status(200).json({
+        success: true,
+        data: radServices
+    });
+});
+
+// Most Requested Medication
+exports.medication = asyncHandler(async (req, res) => {
+    const sixHour = moment().subtract(6, 'hours').utc().toDate();
+    // Patients who requested medicines in the last 6 hours
+    const requestedPharmas = await EDR.aggregate([
+        {
+            $project: {
+                pharmacyRequest: 1
+            },
+        },
+        {
+            $unwind: '$pharmacyRequest',
+        },
+        {
+            $match: { 'pharmacyRequest.createdAt': { $gte: sixHour } }
+        },
+    ]);
+    const items = await Item.aggregate([
+        {
+            $match: { 'cls': 'Medical' }
+        },
+        {
+            $project: {
+                name: 1,
+                itemCode: 1,
+                medClass: 1,
+                scientificName: 1,
+                vendorId: 1
+            },
+        }
+    ])
+    const vendorItems = await Item.populate(items, [
+        {
+            path: 'vendorId',
+            model: 'Vendor',
+            select: 'englishName',
+        }
+    ]);
+
+    for (let i = 0; i < vendorItems.length; i++) {
+        if (requestedPharmas.length > 0) {
+            for (let j = 0; j < requestedPharmas.length; j++) {
+                let items = requestedPharmas[j].pharmacyRequest.item
+
+                for (let k = 0; k < items.length; k++) {
+                    if (vendorItems[i]._id.toString() === items[k].itemId.toString()) {
+                        if (!vendorItems[i].selected) {
+                            vendorItems[i].selected = 1
+                        }
+                        else {
+                            vendorItems[i].selected++
+                        }
+                    }
+                    else if (!vendorItems[i].selected) {
+                        vendorItems[i].selected = 0
+                    }
+                }
+            }
+        }
+        else {
+            vendorItems[i].selected = 0
+        }
+    }
+    res.status(200).json({
+        success: true,
+        data: vendorItems
+    });
+});
+
+// Dashboard Comulative Data.
+exports.dashboard = asyncHandler(async (req, res) => {
+    const sixHour = moment().subtract(6, 'hours').utc().toDate();
+    // Patients who were assigned a chief complaint in the last 6 hours
+    const assignedChiefComplaints = await EDR.aggregate([
+        {
+            $project: {
+                chiefComplaint: 1
+            },
+        },
+        {
+            $unwind: '$chiefComplaint',
+        },
+        {
+            $match: { 'chiefComplaint.assignedTime': { $gte: sixHour } }
+        },
+    ]);
+    const chiefComplaints = await ChiefComplaints.aggregate([
+        {
+            $match: {
+                $and: [
+                    { 'availability': true },
+                    { 'disabled': false },
+                ],
+            },
+        },
+        {
+            $project: {
+                name: 1,
+                chiefComplaintId: 1
+            },
+        },
+    ])
+    for (let i = 0; i < chiefComplaints.length; i++) {
+        if (assignedChiefComplaints.length > 0) {
+            for (let j = 0; j < assignedChiefComplaints.length; j++) {
+                if (chiefComplaints[i]._id.toString() === assignedChiefComplaints[j].chiefComplaint.chiefComplaintId.toString()) {
+                    if (!chiefComplaints[i].selected) {
+                        chiefComplaints[i].selected = 1
+                    }
+                    else {
+                        chiefComplaints[i].selected++
+                    }
+                }
+                else if (!chiefComplaints[i].selected) {
+                    chiefComplaints[i].selected = 0
+                }
+            }
+        }
+        else {
+            chiefComplaints[i].selected = 0
+        }
+    }
+    let totalCC = 0
+    chiefComplaints.map((d) => {
+        totalCC += d.selected
+    })
+
+    // Patients who were assigned a careStream in the last 6 hours
+    const assignedCareStreams = await EDR.aggregate([
+        {
+            $project: {
+                careStream: 1
+            },
+        },
+        {
+            $unwind: '$careStream',
+        },
+        {
+            $match: { 'careStream.assignedTime': { $gte: sixHour } }
+        },
+    ]);
+    const careStreams = await CareStreams.aggregate([
+        {
+            $match: { 'disabled': false },
+        },
+        {
+            $project: {
+                name: 1,
+                identifier: 1
+            },
+        },
+    ])
+    for (let i = 0; i < careStreams.length; i++) {
+        if (assignedCareStreams.length > 0) {
+            for (let j = 0; j < assignedCareStreams.length; j++) {
+                if (careStreams[i]._id.toString() === assignedCareStreams[j].careStream.careStreamId.toString()) {
+                    if (!careStreams[i].selected) {
+                        careStreams[i].selected = 1
+                    }
+                    else {
+                        careStreams[i].selected++
+                    }
+                }
+                else if (!careStreams[i].selected) {
+                    careStreams[i].selected = 0
+                }
+            }
+        }
+        else {
+            careStreams[i].selected = 0
+        }
+    }
+    let totalCS = 0
+    careStreams.map((d) => {
+        totalCS += d.selected
+    })
+
+    // Patients who requested lab service in the last 6 hours
+    const requestedLabReq = await EDR.aggregate([
+        {
+            $project: {
+                labRequest: 1
+            },
+        },
+        {
+            $unwind: '$labRequest',
+        },
+        {
+            $match: { 'labRequest.requestedAt': { $gte: sixHour } }
+        },
+    ]);
+    const labServices = await LabServices.aggregate([
+        {
+            $match: { 'disabled': false },
+        },
+        {
+            $project: {
+                name: 1,
+                identifier: 1,
+                type: 1
+            },
+        },
+    ])
+    for (let i = 0; i < labServices.length; i++) {
+        labServices[i].lastRequested = ''
+        if (requestedLabReq.length > 0) {
+            for (let j = 0; j < requestedLabReq.length; j++) {
+                if (labServices[i]._id.toString() === requestedLabReq[j].labRequest.serviceId.toString()) {
+                    if (!labServices[i].selected) {
+                        labServices[i].selected = 1
+                    }
+                    else {
+                        labServices[i].selected++
+                    }
+                    if (labServices[i].lastRequested = '') {
+                        labServices[i].lastRequested = requestedLabReq[j].labRequest.requestedAt
+                    }
+                    else if (labServices[i].lastRequested <= requestedLabReq[j].labRequest.requestedAt) {
+                        labServices[i].lastRequested = requestedLabReq[j].labRequest.requestedAt
+                    }
+                }
+                else if (!labServices[i].selected) {
+                    labServices[i].selected = 0
+                }
+            }
+        }
+        else {
+            labServices[i].selected = 0
+        }
+    }
+    let totalLabServices = 0
+    labServices.map((d) => {
+        totalLabServices += d.selected
+    })
+
+    // Patients who requested rad service in the last 6 hours
+    const requestedRadReq = await EDR.aggregate([
+        {
+            $project: {
+                radRequest: 1
+            },
+        },
+        {
+            $unwind: '$radRequest',
+        },
+        {
+            $match: { 'radRequest.requestedAt': { $gte: sixHour } }
+        },
+    ]);
+    const radServices = await RadServices.aggregate([
+        {
+            $match: { 'disabled': false },
+        },
+        {
+            $project: {
+                name: 1,
+                identifier: 1,
+                type: 1
+            },
+        },
+    ])
+    for (let i = 0; i < radServices.length; i++) {
+        radServices[i].lastRequested = ''
+        if (requestedRadReq.length > 0) {
+            for (let j = 0; j < requestedRadReq.length; j++) {
+                if (radServices[i]._id.toString() === requestedRadReq[j].radRequest.serviceId.toString()) {
+                    if (!radServices[i].selected) {
+                        radServices[i].selected = 1
+                    }
+                    else {
+                        radServices[i].selected++
+                    }
+                    if (radServices[i].lastRequested = '') {
+                        radServices[i].lastRequested = requestedRadReq[j].radRequest.requestedAt
+                    }
+                    else if (radServices[i].lastRequested <= requestedRadReq[j].radRequest.requestedAt) {
+                        radServices[i].lastRequested = requestedRadReq[j].radRequest.requestedAt
+                    }
+                }
+                else if (!radServices[i].selected) {
+                    radServices[i].selected = 0
+                }
+            }
+        }
+        else {
+            radServices[i].selected = 0
+        }
+    }
+    let totalRadServices = 0
+    radServices.map((d) => {
+        totalRadServices += d.selected
+    })
+
+    // Patients who requested medicines in the last 6 hours
+    const requestedPharmas = await EDR.aggregate([
+        {
+            $project: {
+                pharmacyRequest: 1
+            },
+        },
+        {
+            $unwind: '$pharmacyRequest',
+        },
+        {
+            $match: { 'pharmacyRequest.createdAt': { $gte: sixHour } }
+        },
+    ]);
+    const items = await Item.aggregate([
+        {
+            $match: { 'cls': 'Medical' }
+        },
+        {
+            $project: {
+                name: 1,
+                itemCode: 1,
+                medClass: 1,
+                scientificName: 1,
+                vendorId: 1
+            },
+        }
+    ])
+    const vendorItems = await Item.populate(items, [
+        {
+            path: 'vendorId',
+            model: 'Vendor',
+            select: 'englishName',
+        }
+    ]);
+
+    for (let i = 0; i < vendorItems.length; i++) {
+        if (requestedPharmas.length > 0) {
+            for (let j = 0; j < requestedPharmas.length; j++) {
+                let items = requestedPharmas[j].pharmacyRequest.item
+
+                for (let k = 0; k < items.length; k++) {
+                    if (vendorItems[i]._id.toString() === items[k].itemId.toString()) {
+                        if (!vendorItems[i].selected) {
+                            vendorItems[i].selected = 1
+                        }
+                        else {
+                            vendorItems[i].selected++
+                        }
+                    }
+                    else if (!vendorItems[i].selected) {
+                        vendorItems[i].selected = 0
+                    }
+                }
+            }
+        }
+        else {
+            vendorItems[i].selected = 0
+        }
+    }
+    let totalMedication = 0
+    vendorItems.map((d) => {
+        totalMedication += d.selected
+    })
+
+    res.status(200).json({
+        success: true,
+        data: {
+            totalCC,
+            totalCS,
+            totalLabServices,
+            totalRadServices,
+            totalMedication
+        }
     });
 });
--- routes/adminDashboard.js
+++ routes/adminDashboard.js
@@ -3,18 +3,20 @@ const express = require('express');
 const router = express.Router();
 
 const {
+    dashboard,
     chiefComplaints,
     careStreams,
-    // medication,
+    medication,
     labTests,
-    // radiologyExams
+    radiologyExams
 } = require('../controllers/adminDashboard');
 
 // Admin dashboard API Routes
+router.get('/dashboard', dashboard);
 router.get('/chiefComplaints', chiefComplaints);
 router.get('/careStreams', careStreams);
-// router.get('/medication', medication);
+router.get('/medication', medication);
 router.get('/labTests', labTests);
-// router.get('/radiologyExams', radiologyExams);
+router.get('/radiologyExams', radiologyExams);
 
 module.exports = router;
