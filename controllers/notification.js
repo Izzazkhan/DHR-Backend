b779e2993151843267bf23e76184afc036f27b34
Naeem775 <naeemtahir775@gmail.com>
2021-03-10 13:56:26 +0500

    apis completed for notification

--- controllers/notification.js
+++ controllers/notification.js
@@ -2,6 +2,124 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Notification = require('../models/notification/notification');
 
+exports.notificationCount = asyncHandler(async (req, res, next) => {
+  const count = await Notification.find({
+    'sendTo.userId': req.params.id,
+    'sendTo.read': false,
+  }).countDocuments();
+
+  res.status(200).json({
+    success: true,
+    data: count,
+  });
+});
+
+exports.readNotifications = asyncHandler(async (req, res, next) => {
+  const read = await Notification.find({
+    'sendTo.userId': req.params.id,
+    'sendTo.read': true,
+    sendFrom: req.params.sendFrom,
+  })
+    .populate('sendTo.userId')
+    .populate([
+      {
+        path: 'patient',
+        model: 'EDR',
+        select:
+          'chiefComplaint.chiefComplaintId patientId room.roomId careStream.name ',
+
+        populate: [
+          {
+            path: 'chiefComplaint.chiefComplaintId',
+            model: 'chiefComplaint',
+            select: 'chiefComplaint.chiefComplaintId',
+            populate: [
+              {
+                path: 'productionArea.productionAreaId',
+                model: 'productionArea',
+                select: 'paName',
+              },
+            ],
+          },
+          {
+            path: 'patientId',
+            model: 'patientfhir',
+            select: 'identifier name',
+          },
+          {
+            path: 'room.roomId',
+            model: 'room',
+            select: 'roomNo',
+          },
+        ],
+      },
+      {
+        path: 'roPatient',
+        model: 'patientfhir',
+        select: 'identifier name insuranceNumber',
+      },
+    ])
+    .sort({ $natural: -1 });
+
+  res.status(200).json({
+    success: true,
+    data: read,
+  });
+});
+
+exports.unReadNotifications = asyncHandler(async (req, res, next) => {
+  const unRead = await Notification.find({
+    'sendTo.userId': req.params.id,
+    'sendTo.read': false,
+    sendFrom: req.params.sendFrom,
+  })
+    .populate('sendTo.userId')
+    .populate([
+      {
+        path: 'patient',
+        model: 'EDR',
+        select:
+          'chiefComplaint.chiefComplaintId patientId room.roomId careStream.name ',
+
+        populate: [
+          {
+            path: 'chiefComplaint.chiefComplaintId',
+            model: 'chiefComplaint',
+            select: 'chiefComplaint.chiefComplaintId',
+            populate: [
+              {
+                path: 'productionArea.productionAreaId',
+                model: 'productionArea',
+                select: 'paName',
+              },
+            ],
+          },
+          {
+            path: 'patientId',
+            model: 'patientfhir',
+            select: 'identifier name',
+          },
+          {
+            path: 'room.roomId',
+            model: 'room',
+            select: 'roomNo',
+          },
+        ],
+      },
+      {
+        path: 'roPatient',
+        model: 'patientfhir',
+        select: 'identifier name insuranceNumber',
+      },
+    ])
+    .sort({ $natural: -1 });
+
+  res.status(200).json({
+    success: true,
+    data: unRead,
+  });
+});
+
 exports.getNotification = asyncHandler(async (req, res) => {
   const not = await Notification.find({ 'sendTo.userId': req.params.id })
     .populate('sendTo.userId')
@@ -9,7 +127,8 @@ exports.getNotification = asyncHandler(async (req, res) => {
       {
         path: 'patient',
         model: 'EDR',
-        select: 'chiefComplaint.chiefComplaintId patientId room careStream ',
+        select:
+          'chiefComplaint.chiefComplaintId patientId room.roomId careStream.name ',
 
         populate: [
           {
