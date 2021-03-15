7f1ac9d6240f2648093f95c4274074262d4308c1
Naeem775 <naeemtahir775@gmail.com>
2021-03-15 16:38:34 +0500

    read and unread notifications fixed

--- controllers/notification.js
+++ controllers/notification.js
@@ -28,52 +28,68 @@ exports.notificationCount = asyncHandler(async (req, res, next) => {
 });
 
 exports.readNotifications = asyncHandler(async (req, res, next) => {
-  const read = await Notification.find({
-    'sendTo.userId': req.params.id,
-    'sendTo.read': true,
-    sendFrom: req.params.sendFrom,
-  })
-    .populate('sendTo.userId')
-    .populate([
-      {
-        path: 'patient',
-        model: 'EDR',
-        select:
-          'chiefComplaint.chiefComplaintId patientId room.roomId careStream.name ',
-
-        populate: [
-          {
-            path: 'chiefComplaint.chiefComplaintId',
-            model: 'chiefComplaint',
-            select: 'chiefComplaint.chiefComplaintId',
-            populate: [
-              {
-                path: 'productionArea.productionAreaId',
-                model: 'productionArea',
-                select: 'paName',
-              },
-            ],
-          },
-          {
-            path: 'patientId',
-            model: 'patientfhir',
-            select: 'identifier name',
-          },
-          {
-            path: 'room.roomId',
-            model: 'room',
-            select: 'roomNo',
-          },
+  const unwind = await Notification.aggregate([
+    {
+      $unwind: '$sendTo',
+    },
+    {
+      $match: {
+        $and: [
+          { 'sendTo.userId': mongoose.Types.ObjectId(req.params.id) },
+          { 'sendTo.read': true },
+          { sendFrom: req.params.sendFrom },
         ],
       },
-      {
-        path: 'roPatient',
-        model: 'patientfhir',
-        select: 'identifier name insuranceNumber',
+    },
+    {
+      $sort: {
+        natural: -1,
       },
-    ])
-    .sort({ $natural: -1 });
+    },
+  ]);
+  const read = await Notification.populate(unwind, [
+    {
+      path: 'patient',
+      model: 'EDR',
+      select:
+        'chiefComplaint.chiefComplaintId patientId room.roomId careStream.name ',
 
+      populate: [
+        {
+          path: 'chiefComplaint.chiefComplaintId',
+          model: 'chiefComplaint',
+          select: 'chiefComplaint.chiefComplaintId',
+          populate: [
+            {
+              path: 'productionArea.productionAreaId',
+              model: 'productionArea',
+              select: 'paName',
+            },
+          ],
+        },
+        {
+          path: 'patientId',
+          model: 'patientfhir',
+          select: 'identifier name',
+        },
+        {
+          path: 'room.roomId',
+          model: 'room',
+          select: 'roomNo',
+        },
+      ],
+    },
+    {
+      path: 'roPatient',
+      model: 'patientfhir',
+      select: 'identifier name insuranceNumber',
+    },
+    {
+      path: 'sendTo.userId',
+      model: 'staff',
+      select: 'identifier name',
+    },
+  ]);
   res.status(200).json({
     success: true,
     data: read,
@@ -81,52 +97,68 @@ exports.readNotifications = asyncHandler(async (req, res, next) => {
 });
 
 exports.unReadNotifications = asyncHandler(async (req, res, next) => {
-  const unRead = await Notification.find({
-    'sendTo.userId': req.params.id,
-    'sendTo.read': false,
-    sendFrom: req.params.sendFrom,
-  })
-    .populate('sendTo.userId')
-    .populate([
-      {
-        path: 'patient',
-        model: 'EDR',
-        select:
-          'chiefComplaint.chiefComplaintId patientId room.roomId careStream.name ',
-
-        populate: [
-          {
-            path: 'chiefComplaint.chiefComplaintId',
-            model: 'chiefComplaint',
-            select: 'chiefComplaint.chiefComplaintId',
-            populate: [
-              {
-                path: 'productionArea.productionAreaId',
-                model: 'productionArea',
-                select: 'paName',
-              },
-            ],
-          },
-          {
-            path: 'patientId',
-            model: 'patientfhir',
-            select: 'identifier name',
-          },
-          {
-            path: 'room.roomId',
-            model: 'room',
-            select: 'roomNo',
-          },
+  const unwind = await Notification.aggregate([
+    {
+      $unwind: '$sendTo',
+    },
+    {
+      $match: {
+        $and: [
+          { 'sendTo.userId': mongoose.Types.ObjectId(req.params.id) },
+          { 'sendTo.read': true },
+          { sendFrom: req.params.sendFrom },
         ],
       },
-      {
-        path: 'roPatient',
-        model: 'patientfhir',
-        select: 'identifier name insuranceNumber',
+    },
+    {
+      $sort: {
+        natural: -1,
       },
-    ])
-    .sort({ $natural: -1 });
+    },
+  ]);
+  const unRead = await Notification.populate(unwind, [
+    {
+      path: 'patient',
+      model: 'EDR',
+      select:
+        'chiefComplaint.chiefComplaintId patientId room.roomId careStream.name ',
 
+      populate: [
+        {
+          path: 'chiefComplaint.chiefComplaintId',
+          model: 'chiefComplaint',
+          select: 'chiefComplaint.chiefComplaintId',
+          populate: [
+            {
+              path: 'productionArea.productionAreaId',
+              model: 'productionArea',
+              select: 'paName',
+            },
+          ],
+        },
+        {
+          path: 'patientId',
+          model: 'patientfhir',
+          select: 'identifier name',
+        },
+        {
+          path: 'room.roomId',
+          model: 'room',
+          select: 'roomNo',
+        },
+      ],
+    },
+    {
+      path: 'roPatient',
+      model: 'patientfhir',
+      select: 'identifier name insuranceNumber',
+    },
+    {
+      path: 'sendTo.userId',
+      model: 'staff',
+      select: 'identifier name',
+    },
+  ]);
   res.status(200).json({
     success: true,
     data: unRead,
