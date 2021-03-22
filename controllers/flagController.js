fe345b7e3b53847e1f134a6d0aede461dc12dc60
Naeem775 <naeemtahir775@gmail.com>
2021-03-22 17:42:47 +0500

    in progress status added in query

--- controllers/flagController.js
+++ controllers/flagController.js
@@ -19,7 +19,7 @@ const ErrorResponse = require('../utils/errorResponse');
 
 exports.getAllPendingFlag = asyncHandler(async (req, res, next) => {
   const flag = await Flag.find({
-    status: 'pending',
+    $or: [{ status: 'pending' }, { status: 'in_progress' }],
     generatedFrom: req.params.generatedFrom,
   }).populate([
     {
