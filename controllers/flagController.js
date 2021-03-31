ee1e3820e4e7e8d8183563f93e8af0e4a0251737
Naeem775 <naeemtahir775@gmail.com>
2021-03-31 13:44:02 +0500

    changes im flag for imaging technician

--- controllers/flagController.js
+++ controllers/flagController.js
@@ -20,10 +20,7 @@ const ErrorResponse = require('../utils/errorResponse');
 exports.getAllPendingFlag = asyncHandler(async (req, res, next) => {
   const flag = await Flag.find({
     status: 'pending',
-    $or: [
-      { generatedFor: req.params.staff },
-      { generatedFor: req.params.role },
-    ],
+    $or: [{ generatedFor: req.body.staff }, { generatedFor: req.body.role }],
   }).populate([
     {
       path: 'edrId',
@@ -136,10 +133,7 @@ exports.updateFlag = asyncHandler(async (req, res, next) => {
 exports.getAllCompletedFlag = asyncHandler(async (req, res, next) => {
   const flag = await Flag.find({
     status: 'completed',
-    $or: [
-      { generatedFor: req.params.staff },
-      { generatedFor: req.params.role },
-    ],
+    $or: [{ generatedFor: req.body.staff }, { generatedFor: req.body.role }],
   }).populate([
     {
       path: 'edrId',
