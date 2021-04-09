aa647cbf774f7e8b1d2872a5f3220e56e22d4d07
Naeem775 <naeemtahir775@gmail.com>
2021-04-09 17:17:22 +0500

    çhanges

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,11 +6,13 @@ const {
   logout,
   forgotPassword,
   resetPassword,
+  updatePassword,
 } = require('../controllers/authController');
 
 router.post('/login', login);
 router.post('/logout', logout);
 router.put('/forgotPassword', forgotPassword);
 router.put('/resetPassword', resetPassword);
+router.put('/updatePassword', updatePassword);
 
 module.exports = router;
