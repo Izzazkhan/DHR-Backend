dde4f11814ba95806863f8c8f7913df8f9546e71
Naeem775 <naeemtahir775@gmail.com>
2021-04-26 16:03:54 +0500

    conflicts resolved

--- models/notification/notification.js
+++ models/notification/notification.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
@@ -41,51 +39,3 @@ const notificationSchema = new mongoose.Schema({
   },
 });
 module.exports = mongoose.model('notification', notificationSchema);
-=======
-=======
->>>>>>> 4f26af1c912ada7e85841966a9f754517c018ecb
-const mongoose = require('mongoose');
-
-const notificationSchema = new mongoose.Schema({
-  title: {
-    type: String,
-  },
-  message: {
-    type: String,
-  },
-  route: {
-    type: String,
-  },
-  sendTo: [
-    {
-      userId: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'staff',
-      },
-      read: {
-        type: Boolean,
-        default: false,
-      },
-    },
-  ],
-  patient: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'EDR',
-  },
-  sendFrom: {
-    type: String,
-  },
-  roPatient: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'patientfhir',
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
-module.exports = mongoose.model('notification', notificationSchema);
-<<<<<<< HEAD
->>>>>>> 4f26af1c912ada7e85841966a9f754517c018ecb
-=======
->>>>>>> 4f26af1c912ada7e85841966a9f754517c018ecb
